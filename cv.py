c5d6f88e005f0f9768a5960a456cb45faf554cea
meredithom <meredithom@gmail.com>
2016-11-05 18:59:45 -0700

    Add files via upload
    Gives error: 
    'Traceback (most recent call last):
      File "cv.py", line 22, in <module>
        data = np.ndarray(dataList)
    ValueError: sequence too large; must be smaller than 32
    '

--- cv.py
+++ cv.py
@@ -3,11 +3,28 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import Perceptron, LogisticRegression
+import csv
+import numpy as np
 
-iris = datasets.load_iris()
+#iris = datasets.load_iris()
+
+#data_train, data_test, labels_train, labels_test = train_test_split(
+#	iris.data, iris.target, test_size=0.4, random_state=0)
+
+dataList = []
+with open('Victoria_BC_Police_Department_Data_1000_items.csv','rU') as csv_file:
+	reader = csv.reader(csv_file, delimiter=',')
+	for row in reader:
+		dataList.append(row)
+
+del dataList[0]
+
+data = np.ndarray(dataList)
+
+print data
 
 data_train, data_test, labels_train, labels_test = train_test_split(
-	iris.data, iris.target, test_size=0.4, random_state=0)
+	data.data, data.target, test_size=0.4, random_state=0)
 
 algs = [
 	GaussianNB(), 
@@ -18,7 +35,7 @@ algs = [
 	LogisticRegression(), 
 ]
 
-# Run through each classifier, train them with the training dataset, then test it using the score function
+#Run through each classifier, train them with the training dataset, then test it using the score function
 for alg in algs:
 	alg = alg.fit(data_train, labels_train)
 	print '%s: %f' % (type(alg).__name__, alg.score(data_test, labels_test))
